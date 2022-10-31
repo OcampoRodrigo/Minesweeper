94df3a6a374a9bce84468d3aaa3f2d6737564ab5
Rodrigo Ocampo <ocampoirodrigo@gmail.com>
2022-10-31 20:52:55 -0300

    Added root class

---
+++ main.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+#Creating basic window class. We use name "root" because it is a convention on tkinter so we can search for problems in
+# stackoverflow for example
+root = Tk()
+
+#Iterate until we close it
+root.mainloop()
