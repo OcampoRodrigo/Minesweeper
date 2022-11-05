895c4041a806fe0b12c67e14f5a38b3bc7d3d374
Rodrigo Ocampo <ocampoirodrigo@gmail.com>
2022-11-05 20:32:21 -0300

    Setting properties of the window class

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from tkinter import *
 #Creating basic window class. We use name "root" because it is a convention on tkinter so we can search for problems in
 # stackoverflow for example.
 root = Tk()
+root.configure(bg="lightblue")
+#Setting some properties for the windows
+root.geometry("1440x720")
+root.title("Minesweeper Game")
+root.resizable(False , False)
 
 #Iterate until we close it.
 root.mainloop()
