225d53d26a76aa460cba1b41c617bdedcf8a3431
Rodrigo Ocampo <ocampoirodrigo@gmail.com>
2022-12-11 13:58:10 -0300

    Added methods get_cell_by_axis and show_cell
    get_cell_by_axis takes the x and y axis as parameters and returns the cell object, show cell for now creates a list of surrounding cells (if it is not a mine)

--- cell.py
+++ cell.py
@@ -27,6 +27,26 @@ class Cell:
     def left_click_actions(self, event):
         if self.is_mine:
             self.show_mine()
+        else:
+            self.show_cell()
+    def get_cell_by_axis(self, x, y):
+        #Return a cell object based on the value of x and y
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+
+    def show_cell(self):
+        surrounding_cells = [
+            self.get_cell_by_axis(self.x - 1, self.y - 1),
+            self.get_cell_by_axis(self.x - 1, self.y),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
+            self.get_cell_by_axis(self.x, self.y + 1),
+            self.get_cell_by_axis(self.x + 1, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y),
+            self.get_cell_by_axis(self.x + 1, self.y + 1),
+        ]
+        print(surrounding_cells)
     def show_mine(self):
         #A logic to interrupt the game and display a message that the player lost
         #For now, we only change the background color to red
